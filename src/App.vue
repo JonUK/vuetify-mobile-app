aef8d5cc9ec81fcd1ab23f56703ee7373087b227
JonUK <jonuk2018@jjwebdesign.co.uk>
2019-01-18 22:55:09 +0000

    Resolved linting issues

--- src/App.vue
+++ src/App.vue
@@ -22,17 +22,17 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
   },
-  data () {
+  data() {
     return {
-      //
-    }
+
+    };
   }
-}
+};
 </script>
