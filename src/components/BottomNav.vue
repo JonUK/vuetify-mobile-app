de5d4f164c2f553f0ea4153fc6e79e5cc8238876
JonUK <jonuk2018@jjwebdesign.co.uk>
2019-01-19 16:38:56 +0000

    Added BottomNav component

---
+++ src/components/BottomNav.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <v-bottom-nav app fixed :active.sync="activeItem" :value="true">
+    <v-btn flat color="primary" value="top">
+      <span>Top Stories</span>
+      <v-icon>thumb_up</v-icon>
+    </v-btn>
+
+    <v-btn flat color="primary" value="code">
+      <span>Code Examples</span>
+      <v-icon>code</v-icon>
+    </v-btn>
+
+    <v-btn flat color="primary" value="favorites">
+      <span>Favorites</span>
+      <v-icon>favorite</v-icon>
+    </v-btn>
+
+  </v-bottom-nav>
+
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class BottomNav extends Vue {
+    activeItem: string = 'top';
+  }
+</script>
