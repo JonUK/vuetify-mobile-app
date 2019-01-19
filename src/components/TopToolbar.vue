f28c0b1285c2aaa37fec4ef35c1a27def3de9d09
JonUK <jonuk2018@jjwebdesign.co.uk>
2019-01-19 16:35:04 +0000

    Added TopToolbar component

---
+++ src/components/TopToolbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+
+    <v-navigation-drawer app fixed v-model="showMenu">
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>help</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Help</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+
+    <v-toolbar fixed app dark color="primary">
+
+      <v-toolbar-side-icon @click.stop="toggleMenu"></v-toolbar-side-icon>
+
+      <v-toolbar-title class="white--text">Vue News</v-toolbar-title>
+
+    </v-toolbar>
+
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class TopToolbar extends Vue {
+    showMenu: boolean = false;
+
+    toggleMenu(): void {
+      this.showMenu = !this.showMenu;
+    }
+  }
+</script>
